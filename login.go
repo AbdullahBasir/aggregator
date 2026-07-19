197be3b8102f2a2904e4017bc38c7ffff05e8454
abdullahbasir <abdullah.sbasir@gmail.com>
2026-07-19 09:56:27 +0400

    created a user database, added a registration handler to the login file which registers the user if there is none in the database

--- login.go
+++ login.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
+	"time"
 
-	"github.com/AbdullahBasir/aggregator/internal/config"
+	"github.com/AbdullahBasir/aggregator/internal/database"
+	"github.com/google/uuid"
 )
-
-type state struct {
-	config *config.Config
-}
 
 type command struct {
 	name string
@@ -24,10 +24,50 @@ func handlerLogin(s *state, cmd command) error {
 	if username == "" {
 		return errors.New("could not get username from command")
 	}
-	err := s.config.SetUser(username)
+	_, err := s.db.GetUser(context.Background(), username)
+	if errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("user %s does not exist", username)
+	} else if err != nil {
+		return fmt.Errorf("could not get user: %w", err)
+	}
+
+	err = s.cfg.SetUser(username)
 	if err != nil {
 		return fmt.Errorf("could not set username: %w", err)
 	}
 	fmt.Printf("Username: %s has been set", username)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return errors.New("could not find username")
+	}
+	name := cmd.args[0]
+	if name == "" {
+		return errors.New("could not get username from command")
+	}
+	_, err := s.db.GetUser(context.Background(), name)
+	if err == nil {
+		return errors.New("user already registered")
+	} else if errors.Is(err, sql.ErrNoRows) {
+		data, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      name,
+		})
+		if err != nil {
+			return fmt.Errorf("could not create user: %w", err)
+		}
+
+		err = s.cfg.SetUser(data.Name)
+		if err != nil {
+			return fmt.Errorf("could not set user data: %w", err)
+		}
+		fmt.Printf("User: %s was registered", data.Name)
+	} else {
+		return fmt.Errorf("unexpected db error: %w", err)
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,31 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/AbdullahBasir/aggregator/internal/config"
+	"github.com/AbdullahBasir/aggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
+type state struct {
+	cfg *config.Config
+	db  *database.Queries
+}
+
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("could not read file: %v", err)
 	}
 
-	err = cfg.SetUser("Abdullah")
+	db, err := sql.Open("postgres", cfg.DbUrl)
 	if err != nil {
-		log.Fatalf("could not set username: %v", err)
+		log.Fatalf("could not open database: %v", err)
 	}
 
-	inState := &state{}
-	inState.config = &cfg
+	dbQueries := database.New(db)
+
+	inState := &state{
+		cfg: &cfg,
+		db:  dbQueries,
+	}
 
 	cmdMap := map[string]func(*state, command) error{}
 	inCommands := &commands{
 		cmdNames: cmdMap,
 	}
 	inCommands.register("login", handlerLogin)
+	inCommands.register("register", handlerRegister)
 	commandArgs := os.Args
 	if len(commandArgs) < 2 {
 		log.Fatal("command not found")
